aad3974d6c2373782b62abdd62686e375c3d86d4
IfayetRaj <a.s.m.rajuddin@gmail.com>
2026-06-04 01:24:49 +0600

    fixing some bug

--- src/modules/issues/issues.service.ts
+++ src/modules/issues/issues.service.ts
@@ -49,38 +49,42 @@ const getAllIssuesFromDB = async (
       ...issue,
       reporter: userMap.get(issue.reporter_id),
     }));
-  } catch (error : any) {
+  } catch (error: any) {
     throw error.message;
   }
 };
 
 // getting single user from DB
 const getSingleIssueFromDB = async (issueId: number) => {
-  const issueResult = await pool.query(`SELECT * FROM issues WHERE id = $1`, [
-    issueId,
-  ]);
-  if (!issueResult.rows.length) {
-    throw new Error("Issue not found");
+  try {
+    const issueResult = await pool.query(`SELECT * FROM issues WHERE id = $1`, [
+      issueId,
+    ]);
+    if (!issueResult.rows.length) {
+      throw new Error("Issue not found");
+    }
+    const issue = issueResult.rows[0];
+    const reporterResult = await pool.query(
+      `
+          SELECT id,name,role
+          FROM users
+          WHERE id = $1
+        `,
+      [issue.reporter_id]
+    );
+    return {
+      id: issue.id,
+      title: issue.title,
+      description: issue.description,
+      type: issue.type,
+      status: issue.status,
+      reporter: reporterResult.rows[0],
+      created_at: issue.created_at,
+      updated_at: issue.updated_at,
+    };
+  } catch (error: any) {
+    throw error.message;
   }
-  const issue = issueResult.rows[0];
-  const reporterResult = await pool.query(
-    `
-        SELECT id,name,role
-        FROM users
-        WHERE id = $1
-      `,
-    [issue.reporter_id]
-  );
-  return {
-    id: issue.id,
-    title: issue.title,
-    description: issue.description,
-    type: issue.type,
-    status: issue.status,
-    reporter: reporterResult.rows[0],
-    created_at: issue.created_at,
-    updated_at: issue.updated_at,
-  };
 };
 
 // update issue
